09d53c4c943c18cc1cee9b015b9d4c09186b0c18
956237586 <master@hylstudio.cn>
2024-11-23 09:19:54 +0800

    test only 58

--- projector-server-common/src/main/kotlin/org/jetbrains/projector/server/ProjectorServer.kt
+++ projector-server-common/src/main/kotlin/org/jetbrains/projector/server/ProjectorServer.kt
@@ -309,7 +309,7 @@ class ProjectorServer private constructor(
           icons = window.icons?.map { it as ImageId },
           isShowing = window.target.isShowing,
           zOrder = i,
-          bounds = window.target.bounds.shift(PGraphicsEnvironment.defaultDevice.clientShift),
+          bounds = window.target.shiftBounds(PGraphicsEnvironment.defaultDevice.clientShift),
           headerHeight = window.headerHeight,
           cursorType = window.cursor?.type?.toCursorType(),
           resizable = window.resizable,
